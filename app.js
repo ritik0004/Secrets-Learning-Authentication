63d0b2af3a8f7e275394a56688072bca6ce6e6b0
Ritik Kumar <ritikkumar10058@gmail.com>
2020-07-21 22:48:46 +0530

    Level 1 Authentication using Username and Password

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -21,8 +30,38 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+app.post("/login", function(req, res){
+  User.findOne({email: req.body.username}, function(err, foundUser){
+    if(err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === req.body.password) {
+          res.render("secrets");
+        } else {
+          console.log("Wrong Password");
+        }
+      }
+    }
+  });
+});
+
 app.get("/register", function(req, res){
   res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
 });
 
 app.listen(3000, function(){
